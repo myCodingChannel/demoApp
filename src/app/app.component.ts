0ad43e2f0c07c6260f49b93f7563562fc4907846
kontashi35 <khakontas33@gmail.com>
2020-05-18 17:17:26 +0530

    added google login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,24 +50,6 @@ export class AppComponent implements OnInit {
   facebookLogin(){
    var provider = new firebase.auth.FacebookAuthProvider();
    this.provider = provider;
-
-   firebase.auth().signInWithPopup(provider).then(function(result) {
- 
-    var user = result.user;
-    console.log(user);
-    
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-  
   }
  
   
